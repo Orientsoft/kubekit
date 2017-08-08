016a05ac247a65f76242c23a75decb77ccc23f3a
Timothy <yexiaozhou2003@gmail.com>
2017-08-08 15:57:02 +0800

    add commands

--- main.go
+++ main.go
@@ -25,9 +25,31 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "KubeKit"
-	app.Usage = ""
+	app.Usage = "A kubernetes toolkit for offline deploying K8S & apps."
+	app.Version = "0.1.0"
 	app.Action = func(c *cli.Context) error {
 		return nil
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   "Initialize current server with Docker engine & Kubernetes master.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Initializing...")
+				return nil
+			},
+		},
+		{
+			Name:    "server",
+			Aliases: []string{"s"},
+			Usage:   "Start kubekit toolkit server.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Server is starting...")
+				return nil
+			},
+		},
 	}
 
 	app.Run(os.Args)
