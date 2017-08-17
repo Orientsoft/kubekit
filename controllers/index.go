8b80e4f6f0e63500364bdfb70aaa3ebb7e60058d
Timothy <yexiaozhou2003@gmail.com>
2017-08-17 16:40:36 +0800

    remove debug info

--- controllers/index.go
+++ controllers/index.go
@@ -1,14 +1,12 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (router *MainRouter) IndexHandler(c *gin.Context) {
-	fmt.Println("output:", router.nodeList.Nodes)
 	c.HTML(http.StatusOK, "index.html", gin.H{
 		"Nodes": router.nodeList.Nodes,
 	})
